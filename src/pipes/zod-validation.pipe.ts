63b0ff3131cd93765fb1eeec699544776a42547c
Pedro Brasca <pedropaulobrasca@gmail.com>
2025-03-12 20:30:16 -0300

    feat: controller para criação de perguntas

--- src/pipes/zod-validation.pipe.ts
+++ src/pipes/zod-validation.pipe.ts
@@ -1,8 +1,8 @@
-import { BadRequestException } from '@nestjs/common'
+import { PipeTransform, BadRequestException } from '@nestjs/common'
 import { ZodError, ZodSchema } from 'zod'
 import { fromZodError } from 'zod-validation-error'
 
-export class ZodValidationPipe {
+export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
 
   transform(value: unknown) {
@@ -12,14 +12,13 @@ export class ZodValidationPipe {
       if (error instanceof ZodError) {
         throw new BadRequestException({
           message: 'Validation failed',
+          statusCode: 400,
           errors: fromZodError(error),
-          stausCode: 400,
         })
       }
 
       throw new BadRequestException('Validation failed')
     }
-
     return value
   }
 }
