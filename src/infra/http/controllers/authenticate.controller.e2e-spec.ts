d9b9404d19a730e79babaed4bbbc6205be072171
Pedro Brasca <pedropaulobrasca@gmail.com>
2025-03-15 15:51:24 -0300

    chore: criando a camada de infraestrutura

--- src/infra/http/controllers/authenticate.controller.e2e-spec.ts
+++ src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { AppModule } from '@/app.module'
-import { PrismaService } from '@/prisma/prisma.service'
+import { PrismaService } from '@/infra/prisma/prisma.service'
 import { INestApplication } from '@nestjs/common'
 import { Test } from '@nestjs/testing'
 import { hash } from 'bcryptjs'
 import request from 'supertest'
 import type { Server } from 'http'
+import { AppModule } from '@/infra/app.module'
 
 interface ResponseAuthenticateBody {
   access_token: string
